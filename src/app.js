77cd7a559dc7b0b5221ea69b57ab9fefdadf2773
Natalia Casarin <natalia.casarin02@gmail.com>
2023-12-29 10:57:11 -0600

    improved the UX

--- src/app.js
+++ src/app.js
@@ -15,8 +15,11 @@ function generateAI(response) {
     "You are an expert chef who loves to Christmas. Your mission is to plan a Christmas recipe in basic HTML. Make sure to be precise and detailed with bullet points and show the recipe complete. Make sure to follow the instructions below and give the complete instructions.Make the h1 a little bit smaller. Please! at the end of the recipe and at the  write a Christmas quote and sign with SheCodes AI in bold, ";
   let prompt = `User instructions: Generate a Christmas recipe of ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  instructionsInput.value = "";
   axios.get(apiUrl).then(showResponse);
+  let recipe = document.querySelector("#reponse-generator");
+  recipe.classList.remove("hidden");
+  recipe.innerHTML = `<div class="generating">Generating your Christmas recipe of ${instructionsInput.value}  🎄❤️ !</div>`;
+  instructionsInput.value = "";
 }
 
 let inputGenerator = document.querySelector("#form-generator");
